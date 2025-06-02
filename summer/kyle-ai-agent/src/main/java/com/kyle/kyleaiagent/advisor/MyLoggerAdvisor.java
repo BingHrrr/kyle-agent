ff2d28e43d07837eae6e9b25dea5b1198288be6d
BingHrrr <hrrrWang@gmail.com>
2025-06-02 14:51:54 +0800

    My Own LoggerAdvisor

---
+++ summer/kyle-ai-agent/src/main/java/com/kyle/kyleaiagent/advisor/MyLoggerAdvisor.java
@@ -0,0 +1,67 @@
+package com.kyle.kyleaiagent.advisor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.client.advisor.api.*;
+import org.springframework.ai.chat.model.MessageAggregator;
+import reactor.core.publisher.Flux;
+
+/**
+ * @author Haoran Wang
+ * @date 2025/6/2 11:20
+ */
+
+@Slf4j
+public class MyLoggerAdvisor implements CallAroundAdvisor, StreamAroundAdvisor {
+
+    private int order;
+
+    @Override
+    public String getName() {
+        return this.getClass().getSimpleName();
+    }
+
+    /**
+     * getOrder方法，定义Advisor的优先级，越低优先级越高
+     * @return 优先级
+     */
+    @Override
+    public int getOrder() {
+        return 100;
+    }
+
+    private AdvisedRequest before(AdvisedRequest request) {
+        log.info("AI request: {}", request.userText());
+        return request;
+    }
+
+    private void observeAfter(AdvisedResponse advisedResponse) {
+        log.info("AI response: {}", advisedResponse.response().getResult().getOutput().getText());
+    }
+
+    /**
+     * 同步
+     *
+     * @param advisedRequest
+     * @param chain
+     * @return
+     */
+    public AdvisedResponse aroundCall(AdvisedRequest advisedRequest, CallAroundAdvisorChain chain) {
+        advisedRequest = this.before(advisedRequest);
+        AdvisedResponse advisedResponse = chain.nextAroundCall(advisedRequest);
+        this.observeAfter(advisedResponse);
+        return advisedResponse;
+    }
+
+    /**
+     * 流式
+     *
+     * @param advisedRequest
+     * @param chain
+     * @return
+     */
+    public Flux<AdvisedResponse> aroundStream(AdvisedRequest advisedRequest, StreamAroundAdvisorChain chain) {
+        advisedRequest = this.before(advisedRequest);
+        Flux<AdvisedResponse> advisedResponses = chain.nextAroundStream(advisedRequest);
+        return (new MessageAggregator()).aggregateAdvisedResponse(advisedResponses, this::observeAfter);
+    }
+}
