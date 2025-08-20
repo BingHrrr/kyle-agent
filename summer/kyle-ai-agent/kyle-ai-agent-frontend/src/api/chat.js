f0f5eb2035469afa169edb31121c5e44b6f473a6
BingHrrr <hrrrWang@gmail.com>
2025-08-20 23:54:38 +0800

    frontend & fix

--- summer/kyle-ai-agent/kyle-ai-agent-frontend/src/api/chat.js
+++ summer/kyle-ai-agent/kyle-ai-agent-frontend/src/api/chat.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { createSSEConnection } from '../utils/sse';
 
 // 根据环境变量设置 API 基础 URL
+
 const API_URL = process.env.NODE_ENV === 'production'
     ? 'https://xxx' // 生产环境使用相对路径，适用于前后端部署在同一域名下
     : 'http://localhost:8123/api/ai' // 开发环境指向本地后端服务
